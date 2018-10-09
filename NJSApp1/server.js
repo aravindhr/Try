20b73c66b2a6170b114479ed4ce1b730094dc1a3
Aravindh R <aravindh.r@live.com>
2018-10-09 19:22:37 +0530

    Updated result string with an added line

--- NJSApp1/server.js
+++ NJSApp1/server.js
@@ -4,5 +4,5 @@ var port = process.env.PORT || 1337;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Aravindh!\n');
+    res.end('Hello Aravindh!\n This is the first Node.js application.\n');
 }).listen(port);
